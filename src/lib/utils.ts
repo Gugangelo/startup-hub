327ce89c11505d102c7c4f11479984397975e99f
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-12-15 13:06:10 -0300

    build: Sanity Config

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  return date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  })
+}
